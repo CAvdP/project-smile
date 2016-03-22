6904f9c8d210d5c5239270fd9c30173d37d3abd5
mkMolendijk <markmolendijk@145-24-244-100.wlan.hro.nl>
2016-03-22 10:21:30 +0100

    Updated showPhotos file with correct loop for URLs

--- showPhotos.php
+++ showPhotos.php
@@ -10,3 +10,9 @@ $flickr = new Flickr(FLICKR_KEY, FLICKR_SECRET);
 // Fetch and encode returned data
 $data = $flickr->getPhotos();
 echo json_encode($data);
+
+//Foreach loop for building image url
+
+//foreach ($data['photoset']['photo'] as $img) {
+//    $images = 'https://farm' . $img['farm'] . '.staticflickr.com/' . $img['server'] . '/' . $img['id'] . '_' . $img['secret'] . '.jpg';
+//}
