9f6e36fac00831619bdd9595c03337e5887ad759
Colin <colinabal@hotmail.com>
2016-03-22 12:07:06 +0100

    Added Gallery and changed Flickr.php file

--- showPhotos.php
+++ showPhotos.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once('includes/dbConnect.php');
+//require_once('includes/dbConnect.php');
 require_once('includes/settings.php');
 require_once("includes/classes/Flickr.php");
+
 
 // Init flickr connection
 $flickr = new Flickr(FLICKR_KEY, FLICKR_SECRET);
@@ -11,8 +12,12 @@ $flickr = new Flickr(FLICKR_KEY, FLICKR_SECRET);
 $data = $flickr->getPhotos();
 echo json_encode($data);
 
-//Foreach loop for building image url
-
+//
+////Foreach() loop for building image url
+//
 //foreach ($data['photoset']['photo'] as $img) {
-//    $images = 'https://farm' . $img['farm'] . '.staticflickr.com/' . $img['server'] . '/' . $img['id'] . '_' . $img['secret'] . '.jpg';
+//    $images[] = 'https://farm' . $img['farm'] . '.staticflickr.com/' . $img['server'] . '/' . $img['id'] . '_' . $img['secret'] . '.jpg';
 //}
+//print_r($images);
+
+
