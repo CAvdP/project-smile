7fe140e40ab7a80c5e6cdc22161841b5410884f8
FlorisSchippers <FlorisSchippers@gmail.com>
2016-04-08 13:53:20 +0200

    Added styling to gallery page

--- gallerij.php
+++ gallerij.php
@@ -1,5 +1,5 @@
 <?php
-include_once "database.php";
+include_once "php/database.php";
 
 $sql = "SELECT * FROM fotodata;";
 $photos = [];
@@ -9,6 +9,28 @@ if (isset($result)) {
 		$photos[] = $photo;
 	}
 }
-
-print_r($photos);
 ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Face tracker</title>
+	<meta charset="utf-8">
+	<link href="css/style-gallery.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div id="photoContainer">
+	<?php
+
+	foreach ($photos as $photo) {
+		echo '<img class="gallery-item" src=' . $photo['link'] . '>';
+	}
+
+	?>
+</div>
+</div>
+</body>
+</html>
